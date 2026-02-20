4fea75e70a2ed641cfc9bc080a7b3f3f05959299
kalyan2212 <109565628+kalyan2212@users.noreply.github.com>
2026-02-20 17:22:43 -0500

    Add Terraform variables, backend, outputs

---
+++ terraform/backend.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_version = ">= 1.6"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.90"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.6"
+    }
+  }
+
+  # Remote state in Azure Blob Storage.
+  # Storage account is pre-created by scripts/bootstrap_tfstate.sh
+  # Value is passed via -backend-config in CI.
+  backend "azurerm" {
+    resource_group_name = "tfstate-rg"
+    container_name      = "tfstate"
+    key                 = "downstream.tfstate"
+    # storage_account_name supplied at init time via -backend-config
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
